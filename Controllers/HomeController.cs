75282e6715a3aff0af18e71a58ffd155428431c6
islam <islam@UNKNOWN>
2024-09-26 11:09:29 +0600

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebGrease.Css.Ast.Selectors;
+using CFlow.Models;
+
+namespace CFlow.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult QuestionPost(FormCollection formCollection, string Post)
+        {
+            if (Post == "Post")
+            {
+                if (formCollection["UserQuestion"] == "" || formCollection["QuestionDescription"] == "")
+                    return RedirectToAction("Index", "Home");
+                string postQuestion = formCollection["UserQuestion"].ToString();
+                string postDescription = formCollection["QuestionDescription"].ToString();
+                string postTag = formCollection["tag"].ToString();
+                int result = Questions.questionPost(postQuestion, postDescription, postTag);
+            }
+            return RedirectToAction("Index", "Home");
+        }
+
+        [HttpPost]
+        public ActionResult CommentPost(FormCollection formCollection, string SubComment)
+        { 
+            if (formCollection["AnsComment"] == "")
+                return RedirectToAction("Index", "Home");
+            string AnsCommets = formCollection["AnsComment"].ToString();
+            int QID = Convert.ToInt32(formCollection["QestionID"].ToString());
+            int result = Questions.InsertQuestion(AnsCommets, QID);
+
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
---
+++ Models/Questions.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Data;
+
+namespace CFlow.Models
+{
+    public class Questions
+    {
+        //insert question
+        public static int questionPost(string Question, string Description, string Tags)
+        {
+            int result = 0;
+
+            string ConnString = ConfigurationManager.ConnectionStrings["ConnString"].ConnectionString;
+            SqlConnection sqlConnection = new SqlConnection(ConnString);
+            sqlConnection.Open();
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = sqlConnection;
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.CommandText = "dbo.spQTableIsnertPost";
+            cmd.Parameters.Clear();
+            cmd.Parameters.Add(new SqlParameter("@Question", Question));
+            cmd.Parameters.Add(new SqlParameter("@Description", Description));
+            cmd.Parameters.Add(new SqlParameter("@QTag", Tags));
+            cmd.CommandTimeout = 0;
+            try
+            {
+                result = cmd.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+                string m = ex.Message;
+            }
+
+            cmd.Dispose();
+            sqlConnection.Close();
+
+
+            return result;
+        }
+
+        //insert comment
+        public static int InsertQuestion(string Comment, int ID)
+        {
+            int result = 0;
+
+            string ConnString = ConfigurationManager.ConnectionStrings["ConnString"].ConnectionString;
+            SqlConnection sqlConnection = new SqlConnection(ConnString);
+            sqlConnection.Open();
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = sqlConnection;
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.CommandText = "dbo.spCFlowPostComment";
+            cmd.Parameters.Clear();
+            cmd.Parameters.Add(new SqlParameter("@comment", Comment));
+            cmd.Parameters.Add(new SqlParameter("@QID", ID));
+            cmd.CommandTimeout = 0;
+            try
+            {
+                result = cmd.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+                string m = ex.Message;
+            }
+
+            cmd.Dispose();
+            sqlConnection.Close();
+
+
+            return result;
+        } 
+
+        // read the Question table as data table
+        public static DataTable QuestionTable()
+        {
+            DataTable dataTable = new DataTable();
+
+            string ConnString = ConfigurationManager.ConnectionStrings["ConnString"].ConnectionString;
+            SqlConnection sqlConnection = new SqlConnection(ConnString);
+            sqlConnection.Open();
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = sqlConnection;
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.CommandText = "dbo.spCflowSelectQuestion";
+            cmd.Parameters.Clear();
+            cmd.CommandTimeout = 0;
+            SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+            adapter.Fill(dataTable);
+
+
+            cmd.Dispose();
+            sqlConnection.Close();
+            return dataTable;
+
+        }
+
+        // to read comment datatabel
+        public static DataTable CommentTable()
+        {
+            DataTable dataTable = new DataTable();
+
+            string ConnString = ConfigurationManager.ConnectionStrings["ConnString"].ConnectionString;
+            SqlConnection sqlConnection = new SqlConnection(ConnString);
+            sqlConnection.Open();
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = sqlConnection;
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.CommandText = "dbo.spCflowSelectComment";
+            cmd.Parameters.Clear();
+            cmd.CommandTimeout = 0;
+            SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+            adapter.Fill(dataTable);
+
+
+            cmd.Dispose();
+            sqlConnection.Close();
+            return dataTable;
+
+        }
+    }
+}
